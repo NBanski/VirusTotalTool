f646a34b6b52389ffcb7e7dbfcc0d5655a320904
NBanski <n.d.banski@gmail.com>
2020-10-15 12:48:42 +0200

    commit

--- virustotaltool/__init__.py
+++ virustotaltool/__init__.py
@@ -1,5 +1,10 @@
 import tkinter as tk
-from interface/mainWindow import mainWindow
+from interface.mainWindow import mainWindow
 
 if __name__ == "__main__":
-    # Do stuff.
+    root = tk.Tk()
+    main = mainWindow(root)
+    title = root.title("VirusTotalTool")
+    main.pack(side="top", fill="both", expand=True)
+    root.resizable(0, 0)
+    root.mainloop()
