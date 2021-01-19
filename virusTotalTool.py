373e35ece52de56f9f795b5b5a32abe15407a582
NBanski <n.d.banski@gmail.com>
2021-01-19 10:14:39 +0100

    gui segmentation

--- virusTotalTool.py
+++ virusTotalTool.py
@@ -4,14 +4,13 @@ from modules.local.settings import startConfigFiles, startDatabase
 # To do: create database configuration file to be used during second and later logins.
 # It should be created on first login.
 
-# if __name__ == "__main__":
-#     first_start()
-#     root = tk.Tk()
-#     main = main_window(root)
-#     title = root.title("Virus Total API Tool")
-#     main.pack(side="top", fill="both", expand=True)
-#     root.resizable(0, 0)
-#     root.mainloop()
+if __name__ == "__main__":
+    startConfigFiles()
+    startDatabase()    
+    root = tk.Tk()
+    main = mainWindow(root)
+    title = root.title("Virus Total API Tool")
+    main.pack(side="top", fill="both", expand=True)
+    root.resizable(0, 0)
+    root.mainloop()
 
-startConfigFiles()
-startDatabase()
