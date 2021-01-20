dd1bf4c9763b9913f5fbf5b4b57c3d628bbd3bed
NBanski <n.d.banski@gmail.com>
2021-01-20 15:14:02 +0100

    cleanups

--- virusTotalTool.py
+++ virusTotalTool.py
@@ -3,9 +3,6 @@ import tkinter as tk
 from modules.local.settings import startConfigFiles, startDatabase
 from modules.gui.mainWindow import mainFrame
 
-# To do: create database configuration file to be used during second and later logins.
-# It should be created on first login.
-
 if __name__ == "__main__":
     startConfigFiles()
     startDatabase()    
